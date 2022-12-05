fbc6b8883c0b56e90c41ecdb99263c07e0b870c3
Bakhtiar Abid <70568315+bakhtiarabid@users.noreply.github.com>
2022-12-05 14:34:30 +0600

    Login Page

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ import AuthProvider from './contexts/AuthProvider';
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import Header from './Layouts/Navbar/Header';
 import Test from './Layouts/Navbar/Test';
+import Login from './Login/Login';
 
 
 function App() {
   return (
      <>
-        {/* <AuthProvider>
-       
-        </AuthProvider> */}
-        <Router>
-           <Routes>
-              {/* <PrivateRoute path="/dashboard">
+        <AuthProvider>
+           <Router>
+              <Routes>
+                 {/* <PrivateRoute path="/dashboard">
                     <Dashboard />
                  </PrivateRoute> */}
-              <Route
-                 path="/*"
-                 element={<Navigate to="/navbar"></Navigate>}
-              ></Route>
-              <Route path="/navbar" element={<Header />}></Route>
-              <Route path="/test" element={<Test />}></Route>
-           </Routes>
-           {/* <Footer></Footer> */}
-        </Router>
+                 {/* <Route
+                    path="/*"
+                    element={<Navigate to="/login"></Navigate>}
+                 ></Route> */}
+                 <Route path="/navbar" element={<Header />}></Route>
+                 <Route path="/" element={<Login></Login>}></Route>
+              </Routes>
+              {/* <Footer></Footer> */}
+           </Router>
+        </AuthProvider>
      </>
   );
 }
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,64 @@
+import {
+   getAuth,
+   signInWithPopup,
+   GoogleAuthProvider,
+   signOut,
+   onAuthStateChanged,
+} from "firebase/auth";
+import { useState, useEffect } from "react";
+import initializeAuthentication from "../Login/Firebase/firebase.init";
+
+initializeAuthentication();
+
+const useFirebase = () => {
+   const [user, setUser] = useState({});
+   const [isLoading, setIsLoading] = useState(true);
+
+   const auth = getAuth();
+   const signInUsingGoogle = () => {
+      setIsLoading(true);
+      const googleProvider = new GoogleAuthProvider();
+      return signInWithPopup(auth, googleProvider).finally(() =>
+         setIsLoading(false)
+      );
+   };
+//    const signInUsingGoogle = () => {
+
+//       signInWithPopup(auth, googleProvider)
+//          .then((result) => {
+//             setUser(result.user);
+//          })
+//          .finally(() => setIsLoading(false));
+//    };
+
+   // observe user state change
+   useEffect(() => {
+      const unsubscribed = onAuthStateChanged(auth, (user) => {
+         if (user) {
+            setUser(user);
+         } else {
+            setUser({});
+         }
+         setIsLoading(false);
+      });
+      return () => unsubscribed;
+   }, []);
+   // return signInWithPopup(auth, googleProvider).finally(() =>
+   //          setIsLoading(false)
+
+   const logOut = () => {
+      setIsLoading(true);
+      signOut(auth)
+         .then(() => {})
+         .finally(() => setIsLoading(false));
+   };
+
+   return {
+      user,
+      isLoading,
+      signInUsingGoogle,
+      logOut,
+   };
+};
+
+export default useFirebase;
